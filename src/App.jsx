1a03daad302fcc55ca9a00f7dafe1dcaf18c715a
abufaheema <oye1965@gmail.com>
2023-04-02 16:53:11 +0100

    innitial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "bootstrap/dist/js/bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import React from "react";
+import './sass/main.scss'
+
+import { Box } from "@mui/material";
+
+const App = () => {
+  return (
+    <Box>
+      <h1 className="text-info">hello</h1>
+    </Box>
+  );
+};
+
+export default App;
